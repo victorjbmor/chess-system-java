3c7597129d7d4477156c361182c039d3ec7848c8
Joao Moraes <victorjbmoraes@gmail.com>
2026-06-01 10:19:30 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -48,6 +48,20 @@ public class Board {
 		piece.position = position;
 	}
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position does not exist");
+		}
+		if (piece(position) == null) {
+			return null;
+	
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -62,4 +76,6 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
+	
 }
